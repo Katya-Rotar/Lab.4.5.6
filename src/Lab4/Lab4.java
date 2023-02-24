98f5d915b86962855f4e4b2dffbdfada6e363ccb
user <katya.rotar02@gmail.com>
2023-02-24 23:16:35 +0200

    Lab4.exercise4/5/6

--- src/Lab4/Lab4.java
+++ src/Lab4/Lab4.java
@@ -17,6 +17,9 @@ public class Lab4 {
                 case 1 -> exercise1(i);
                 case 2 -> exercise2(i);
                 case 3 -> exercise3(i);
+                case 4 -> exercise4(i);
+                case 5 -> exercise5(i);
+                case 6 -> exercise6(i);
                 default -> System.out.println("Такої дії не існує");
             }
         }
@@ -73,5 +76,113 @@ public class Lab4 {
         }
         else
             System.out.println("Ця коробка НЕ поміститься");
+    }
+    private static void exercise4(Scanner i){
+        System.out.println("Тестування");
+        int points = 0;
+        System.out.println("У якому році було проголошено незалежність України ?");
+        int question1 = i.nextInt();
+        if(question1 == 1991){
+            points++;
+        }
+        System.out.println("У якому році було ухвалено Конституцію України ?");
+        int question2 = i.nextInt();
+        if(question2 == 1996){
+            points++;
+        }
+        System.out.println("У якому році було обрано першого призидента України ?");
+        int question3 = i.nextInt();
+        if(question3 == 1994){
+            points++;
+        }
+        System.out.println("У якому році було запроваджено національну грошову одиницю - гривня ?");
+        int question4 = i.nextInt();
+        if(question4 == 1996){
+            points++;
+        }
+        System.out.println("У якому році було започатковано Збройні Сили України ?");
+        int question5 = i.nextInt();
+        if(question5 == 1991){
+            points++;
+        }
+        int point = (points * 100) / 5;
+        System.out.println( points + " / 5\n" + "Це " + point +"%");
+    }
+    private static void exercise5(Scanner i){
+        System.out.println("Введіть порядковий номер дня");
+        int day = i.nextInt();
+        switch (day){
+            case 1 -> System.out.println("""
+                        Понеділок
+                        1. Вища математика
+                        2. Алгоритиізація та програмування
+                        3. Алгоритиізація та програмування
+                        """);
+            case 2 -> System.out.println("""
+                    Вівторок
+                    1. Структури даних та алгоритмів
+                    2. Комп'ютерна графіка
+                    3. -
+                    4. -
+                    5. Фізичне виховання
+                    """);
+            case 3 -> System.out.println("""
+                        Середа
+                        1. -
+                        2. Вища метематика
+                        3. Алгоритиізація та програмування
+                        """);
+            case 4 -> System.out.println("""
+                        Четвер
+                        1. Фізика
+                        2. Фізика
+                        3. Фізика
+                        """);
+            case 5 -> System.out.println("""
+                        П'ятниця
+                        1. Вища математика
+                        2. Структури даних та алгоритмів
+                        3. Іноземна мова
+                        """);
+            default -> System.out.println("Такого дня не існує");
+        }
+    }
+    private static void exercise6(Scanner i){
+        System.out.print("""
+                Арифметичні дії:
+                1 – додавання
+                2 – віднімання
+                3 - множення
+                4 – ділення
+                Введіть номер арифметичної дії
+                """);
+        int number = i.nextInt();
+        System.out.println("Введіть два дійсні числа");
+        int A = i.nextInt();
+        int B = i.nextInt();
+        switch (number){
+            case 1 -> {
+                int action1 = A + B;
+                System.out.println(action1);
+            }
+            case 2 -> {
+                int action2 = A - B;
+                System.out.println(action2);
+            }
+            case 3 -> {
+                int action3 = A * B;
+                System.out.println(action3);
+            }
+            case 4 -> {
+                if (B == 0){
+                    System.out.println("На 0 ділити не можна");
+                }
+                else {
+                    int action4 = A / B;
+                    System.out.println(action4);
+                }
+            }
+            default -> System.out.println("Такої дії не існує");
+        }
     }
 }
