c8a60b050275c64ab0de4a43539e75a08c1a8a92
user <katya.rotar02@gmail.com>
2023-02-25 19:46:14 +0200

    Lab6.exercise4/5/6

--- src/Lab6/Lab6.java
+++ src/Lab6/Lab6.java
@@ -17,6 +17,9 @@ public class Lab6 {
                 case 1 -> exercise1(i);
                 case 2 -> exercise2(i);
                 case 3 -> exercise3(i);
+                case 4 -> exercise4(i);
+                case 5 -> exercise5(i);
+                case 6 -> exercise6(i);
                 default -> System.out.println("Такої дії не існує");
             }
         }
@@ -59,5 +62,70 @@ public class Lab6 {
             }
         }
         System.out.println(sum);
+    }
+    private static void exercise4(Scanner i){
+        System.out.println("""
+                Введіть n цілих чисел
+                Щоб завершити введіть 0
+                """);
+        int exid = 1;
+        int min = 100000;
+        int max = 0;
+        while (exid != 0){
+            int n = i.nextInt();
+            if (n == 0){
+                exid--;
+            }
+            else {
+                if (n <= min) {
+                    min = n;
+                }
+                if (n >= max) {
+                    max = n;
+                }
+            }
+        }
+        int m_n = max - min;
+        System.out.println(m_n);
+    }
+    private static void exercise5(Scanner i){
+        System.out.println("Введіть максимальне можливе значення");
+        int n = i.nextInt();
+        System.out.println("Введіть послідовність цілих чисел");
+        int exid = 0;
+        int sum = 0;
+        int N = 0;
+        while (exid != n) {
+            int n1 = i.nextInt();
+            if (n1 >= 0) {
+                if (n1 == n) {
+                    exid = n;
+                } else {
+                    sum += n1;
+                    N++;
+                }
+            }
+        }
+        int number = sum / N;
+        System.out.println(number);
+    }
+    private static void exercise6(Scanner i){
+        System.out.println("Введіть кількість студентів");
+        int n = i.nextInt();
+        int N = 1;
+        int sum = 0 ;
+        int N2 = 0;
+        System.out.println("Введіть бали ");
+        while (N <= n){
+            int student = i.nextInt();
+            sum += student;
+            if(student == 5){
+                N2++;
+            }
+            N++;
+        }
+        double student2 = (double) sum / n;
+        System.out.println("Середній бал: " + student2);
+        System.out.println("Кількість п'ятірок: "+N2);
     }
 }
