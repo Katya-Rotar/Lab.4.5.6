6de75af6b784d90dc6848e0471132b752b1e2d45
user <katya.rotar02@gmail.com>
2023-02-25 18:28:00 +0200

    Lab6.exercise1/2/3

--- src/Lab6/Lab6.java
+++ src/Lab6/Lab6.java
@@ -14,8 +14,50 @@ public class Lab6 {
             int start = i.nextInt();
             switch (start) {
                 case 0 -> cycle = false;
+                case 1 -> exercise1(i);
+                case 2 -> exercise2(i);
+                case 3 -> exercise3(i);
                 default -> System.out.println("Такої дії не існує");
             }
         }
+    }
+    private static void exercise1(Scanner i){
+        System.out.println("Введіть поточний курс");
+        int currency = i.nextInt();
+        int x = 1;
+        while (x <= 10){
+            int currency1 = x * currency;
+            System.out.println( x + " $ - " + currency1 + " ₴");
+            x++;
+        }
+    }
+    private static void exercise2(Scanner i){
+        System.out.println("Введіть число а");
+        int a = i.nextInt();
+        int max = 0;
+        int n = 1;
+        while ( n < 5000){
+            if( n % a == 0){
+                max = n;
+            }
+            n++;
+        }
+        System.out.println(max);
+    }
+    private static void exercise3(Scanner i){
+        System.out.println("""
+                Введіть n цілих чисел
+                Щоб завершити введіть 0
+                """);
+        int exid = 1;
+        int sum = 0;
+        while (exid != 0){
+            int n = i.nextInt();
+            sum += n;
+            if (n == 0){
+                exid--;
+            }
+        }
+        System.out.println(sum);
     }
 }
